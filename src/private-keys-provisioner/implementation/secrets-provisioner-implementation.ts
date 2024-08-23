ad3a4f78ed554c26fad3864ffae18fd5af70262b
El-Fitz <8971906+El-Fitz@users.noreply.github.com>
2024-08-23 16:33:13 +0200

    feat(provisioner): only refresh private keys if they have expired

--- src/private-keys-provisioner/implementation/secrets-provisioner-implementation.ts
+++ src/private-keys-provisioner/implementation/secrets-provisioner-implementation.ts
@@ -1,6 +1,7 @@
 import { API } from "..";
-import * as luxon from "luxon";
+import { JWK } from "jose";
 import * as JWKSFactory from "../../jwks-factory";
+import * as luxon from "luxon";
 
 export interface Dependencies {
   accessTokenPrivateKeyStore: API.PrivateKeyStore;
@@ -26,27 +27,52 @@ export const create = (deps: Dependencies) => (props: Props): API.SecretsProvisi
   return {
     provision: async () => {
       const now = luxon.DateTime.now();
-      const refreshInterval = luxon.Duration.fromObject({ days: privateKeysRefreshIntervalInDays });
-  
+
+      // 1. Retrieve the latest version creation date for the access token and refresh token private keys
       const latestAccessTokenVersionCreationDate = await accessTokenPrivateKeyStore.retrieveLatestVersionCreationDate();
       const latestRefreshTokenVersionCreationDate = await refreshTokenPrivateKeyStore.retrieveLatestVersionCreationDate();
+      const refreshInterval = luxon.Duration.fromObject({ days: privateKeysRefreshIntervalInDays });
 
+      // 2. Check if the keys are still valid
+      const accessTokenKeyStillValid = latestAccessTokenVersionCreationDate && 
+          now.minus(refreshInterval) <= luxon.DateTime.fromJSDate(latestAccessTokenVersionCreationDate);
+
+      const refreshTokenKeyStillValid = latestRefreshTokenVersionCreationDate && 
+        now.minus(refreshInterval) <= luxon.DateTime.fromJSDate(latestRefreshTokenVersionCreationDate);
+
+
+      if (accessTokenKeyStillValid && refreshTokenKeyStillValid) {
+        // If both keys are still valid, skip updating the keys.
+        return;
+      }
+
+      // 3. Create new keys
       const { accessToken, refreshToken } = await jwksFactory.create();
-      // 1. Store the private key in AWS Secrets Manager
 
-      await accessTokenPrivateKeyStore.storeKey({
-        kid: accessToken.keyID,
-        privateKey: accessToken.privateKey,
-      });
+      let jwks: JWK[] = [];
 
-      // 2. Store the private key in AWS Secrets Manager
-      await refreshTokenPrivateKeyStore.storeKey({
-        kid: refreshToken.keyID,
-        privateKey: refreshToken.privateKey,
-      });
+      // 4. If the access token key has expired, store the private key in AWS Secrets Manager
+      if (!accessTokenKeyStillValid) {
+        await accessTokenPrivateKeyStore.storeKey({
+          kid: accessToken.keyID,
+          privateKey: accessToken.privateKey,
+        });
+        jwks = jwks.concat(accessToken.jwk);
+      }
 
-      // 3. Store the JWKs in DynamoDB
-      await jwksRepository.store({ jwks: [accessToken.jwk, refreshToken.jwk] });
+      // 5. If the refresh token key has expired, store the private key in AWS Secrets Manager
+      if (!refreshTokenKeyStillValid) {
+        await refreshTokenPrivateKeyStore.storeKey({
+          kid: refreshToken.keyID,
+          privateKey: refreshToken.privateKey,
+        });
+        jwks = jwks.concat(refreshToken.jwk);
+      }
+
+      // 6. If there are new keys, store them in the JWKS repository
+      if (jwks.length > 0) {
+        await jwksRepository.store({ jwks: [accessToken.jwk, refreshToken.jwk] });
+      }
     },
   };
 };
